32f23b18380d8f03ce67db1998d6385ffd1e18e1
Baxitjan Bayniyazov <chwalkzz01@gmail.com>
2022-09-05 22:53:56 +0500

    Добавьте файлы проекта.

---
+++ BookPrees/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookPrees
+{
+    public partial class Form1 : Form
+    {  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n, m;
+        string F = "";
+        string B = "";
+        string F2 = "";
+        string B2 = "";
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+              
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            F = "";
+            B = "";
+            F2 = "";
+            B2 = "";
+            shaqiriw();
+        }
+        
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void shaqiriw()
+        {
+            int n = int.Parse(textBox1.Text);
+            int m = int.Parse(numericUpDown1.Text);
+
+            for (int i = 0; i < n / 2; i += 2)
+            {
+                F += n - i + ",";
+                F += m + i + ",";
+            }
+            for (int i = 0; i < n / 2; i += 2)
+            {
+                B += n / 2 - i + ",";
+                B += n / 2 + 1 + i + ",";
+            }
+            for (int i = 0; i < n / 2; i += 2)
+            {
+                F2 += n - i + ",";
+                F2 += m + i + ",";
+            }
+            /* for (int i = 0; i < n / 2; i += 2)
+             {
+                 B2 += n / 2 - i - 2 + ",";
+                 B2 += n / 4 + 1 + i + 2 + ",";
+             }*/
+            if (n % 8 == 0)
+            {
+                for (int i = (m + n / 2 - 3), j = m + n / 2 + 2, k = m + n / 2 - 1, l = m + n / 2; (i >= m - 1) && (j <= m + n - 2) && (k >= m + 3) && (l <= m + n - 4); i = i - 4, j = j + 4, k = k - 4, l = l + 4)
+                {
+                    B2 += i + ",";
+                    B2 += j + ",";
+                    B2 += k + ",";
+                    if (l == (m + n - 4))
+                        B2 += l.ToString();
+                    else
+                        B2 += l + ",";
+                }
+            }
+            textBox2.Text = F;
+            textBox3.Text = B;
+            textBox4.Text = F2;
+            textBox5.Text = B2;
+        }
+    }
+}
---
+++ BookPrees/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("BookPrees")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookPrees")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a433fccf-f18c-4613-90c4-d5b7304abe86")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
